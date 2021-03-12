4705a9fb25a924927b9d1d3ca83bfb762c4806ff
tiger1016 <applejuicy1016@gmail.com>
2021-03-12 23:48:23 +0800

    added fonts

--- src/theme/mapping.js
+++ src/theme/mapping.js
@@ -12,34 +12,34 @@ export function actuatedNormalize(size) {
 
 export default {
   strict: {
-    'text-font-family': 'OpenSans-Regular',
+    'text-font-family': 'Poppins-Regular',
 
     'text-heading-1-font-size': actuatedNormalize(36),
     'text-heading-1-font-weight': '800',
-    'text-heading-1-font-family': 'OpenSans-Bold',
+    'text-heading-1-font-family': 'Poppins-Bold',
 
     'text-heading-5-font-size': actuatedNormalize(24),
     'text-heading-5-font-weight': '700',
-    'text-heading-5-font-family': 'OpenSans-Bold',
+    'text-heading-5-font-family': 'Poppins-Bold',
 
     'text-heading-6-font-size': actuatedNormalize(20),
     'text-heading-6-font-weight': '800',
-    'text-heading-6-font-family': 'OpenSans-Bold',
+    'text-heading-6-font-family': 'Poppins-Bold',
 
     'text-subtitle-1-font-size': actuatedNormalize(15),
     'text-subtitle-1-font-weight': '600',
-    'text-subtitle-1-font-family': 'OpenSans-SemiBold',
+    'text-subtitle-1-font-family': 'Poppins-SemiBold',
 
     'text-paragraph-1-font-size': actuatedNormalize(15),
     'text-paragraph-1-font-weight': '400',
-    'text-paragraph-1-font-family': 'OpenSans-Regular',
+    'text-paragraph-1-font-family': 'Poppins-Regular',
 
     'text-caption-1-font-size': actuatedNormalize(12),
     'text-caption-1-font-weight': '400',
-    'text-caption-1-font-family': 'OpenSans-Regular',
+    'text-caption-1-font-family': 'Poppins-Regular',
 
     'text-label-font-size': actuatedNormalize(12),
     'text-label-font-weight': '800',
-    'text-label-font-family': 'OpenSans-Bold',
+    'text-label-font-family': 'Poppins-Bold',
   },
 };
